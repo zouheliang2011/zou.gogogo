c81bc8ab9e590b17ca7d60a5b890344e7ccd6257
zouheliang2011 <zouhekun@163.com>
2018-06-16 17:59:57 +0800

    增加配置文件访问方式
    springboot中如果不想用注解，想拿着别人已经写好的代码和配置文件直接使用，可以采取这种方式。

--- zou.springboot.gogogo/src/main/java/zou/springboot/gogogo/controller/UserController.java
+++ zou.springboot.gogogo/src/main/java/zou/springboot/gogogo/controller/UserController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import zou.springboot.gogogo.bean.XmlPropertyTest;
+import zou.springboot.gogogo.service.SpringApplicationContext;
 import zou.springboot.gogogo.service.UserInfoService;
 
 
@@ -20,11 +22,48 @@ public class UserController {
 
 	@Autowired
 	private UserInfoService userService;
+	@Autowired
+	private SpringApplicationContext scontext;
+	
 	@RequestMapping(value = "/getname", method = RequestMethod.GET)
-	public String sensitiveIgnorBlank(@PathParam(value = "id") String id) {
+	public String getname(@PathParam(value = "id") String id) {
 		String userNameById = userService.getUserNameById(id);
 		System.out.println(userNameById);
 		return userNameById;
+//		return keyWordSet.sensitiveWordSearch(req.getNick(), source);
+	}
+	
+	@RequestMapping(value = "/testbean", method = RequestMethod.GET)
+	public void testbean() {
+		try
+		{
+			XmlPropertyTest bean = (XmlPropertyTest)SpringApplicationContext.getBean("customerRepositoryFoo");
+			System.out.println(bean.getConfigName());
+			
+		}
+		catch (Exception e)
+		{
+			System.out.println("error1");
+		}
+		try
+		{
+			XmlPropertyTest bean2 = SpringApplicationContext.getBean(XmlPropertyTest.class);
+			System.out.println(bean2.getConfigName());
+			
+		}
+		catch (Exception e)
+		{
+			System.out.println("error2");
+		}
+		try
+		{
+			XmlPropertyTest bean3 = (XmlPropertyTest)SpringApplicationContext.getBean("zouggg");
+			System.out.println(bean3.getConfigName());
+		}
+		catch (Exception e)
+		{
+			System.out.println("error3");
+		}
 //		return keyWordSet.sensitiveWordSearch(req.getNick(), source);
 	}
 }
