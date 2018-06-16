da70e47a7a797d6b94b9c20d867874b445093dd6
zouheliang2011 <zouhekun@163.com>
2018-06-16 18:39:54 +0800

    重新提交

--- zou.springboot.gogogo/src/main/java/zou/springboot/Application.java
+++ zou.springboot.gogogo/src/main/java/zou/springboot/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 import zou.springboot.gogogo.bean.LoginConfiguration;
 
@@ -19,6 +20,7 @@ import zou.springboot.gogogo.bean.LoginConfiguration;
 //如果这个不注释那么就会包找不到Consider defining a bean of type 'xxxMapper' in your configuration
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @EnableConfigurationProperties({LoginConfiguration.class})
+@ImportResource(locations= {"classpath:spring/applicationContext.xml"})
 @MapperScan("zou.springboot.gogogo.mapper")
 public class Application {
 
--- zou.springboot.gogogo/src/main/java/zou/springboot/gogogo/controller/UserController.java
+++ zou.springboot.gogogo/src/main/java/zou/springboot/gogogo/controller/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import zou.springboot.gogogo.bean.XmlPropertyTest;
+import zou.springboot.gogogo.service.SpringApplicationContext;
 import zou.springboot.gogogo.service.UserInfoService;
 
 
@@ -23,6 +25,41 @@ public class UserController {
 		String userNameById = userService.getUserNameById(id);
 		System.out.println(userNameById);
 		return userNameById;
+//		return keyWordSet.sensitiveWordSearch(req.getNick(), source);
+	}
+	
+
+	@RequestMapping(value = "/testbean", method = RequestMethod.GET)
+	public void testbean() {
+		try
+		{
+			XmlPropertyTest bean = (XmlPropertyTest)SpringApplicationContext.getBean("customerRepositoryFoo");
+			System.out.println(bean.getConfigName());
+			
+		}
+		catch (Exception e)
+		{
+			System.out.println("error1");
+		}
+		try
+		{
+			XmlPropertyTest bean2 = SpringApplicationContext.getBean(XmlPropertyTest.class);
+			System.out.println(bean2.getConfigName());
+			
+		}
+		catch (Exception e)
+		{
+			System.out.println("error2");
+		}
+		try
+		{
+			XmlPropertyTest bean3 = (XmlPropertyTest)SpringApplicationContext.getBean("zouggg");
+			System.out.println(bean3.getConfigName());
+		}
+		catch (Exception e)
+		{
+			System.out.println("error3");
+		}
 //		return keyWordSet.sensitiveWordSearch(req.getNick(), source);
 	}
 }
